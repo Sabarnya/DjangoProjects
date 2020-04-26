d38211c5d520fdac7f087fa1f99ceb18611a0042
Sabarnya <sunnyhowrah@gmail.com>
2020-04-26 16:20:56 +0200

    Add existing file

--- First_new_project/testtravello/views.py
+++ First_new_project/testtravello/views.py
@@ -4,27 +4,6 @@ from .models import Destination
 
 def index(request):
 
-    dest1=Destination()
-    dest1.name='Maldives'
-    dest1.description='A gateway to heaven'
-    dest1.img= 'destination_2.jpg'
-    dest1.price=650
-    dest1.offer= True
-
-    dest2=Destination()
-    dest2.name='Brooklyn'
-    dest2.description="It's a dream to be lived"
-    dest2.img= 'destination_3.jpg'
-    dest2.price=850
-    dest2.offer= False
-
-    dest3=Destination()
-    dest3.name='Paris'
-    dest3.description='A city of love'
-    dest3.img='destination_1.jpg'
-    dest3.price=450
-    dest3.offer= True 
-
-    dests=[dest1,dest2,dest3]
+    dests=Destination.objects.all()
 
     return render(request,'index.html',{'dests':dests})
